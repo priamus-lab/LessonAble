9a583f4812503b05cc14e8cbbf3d0e5d94a83d53
Ciro Sannino <ciro.sannino9@studenti.unina.it>
2021-11-14 22:27:39 +0100

    // FIX: - edit of the bestfeame detection

--- Tests/Image To Sequence/Best Frames Detection/bestframesdetection.py
+++ Tests/Image To Sequence/Best Frames Detection/bestframesdetection.py
@@ -16,8 +16,30 @@ from PIL import Image
 
 """**Detect scene content changes in a video**"""
 
-def detectDifferentScenes(videoURL, outputURL):
-  !scenedetect -i {videoURL} -o outputURL -s videoStats.csv detect-content list-scenes save-images
+from scenedetect import VideoManager
+from scenedetect import SceneManager
+from scenedetect.scene_manager import save_images
+
+# For content-aware scene detection:
+from scenedetect.detectors import ContentDetector
+
+def find_scenes(video_path, outputURL, threshold=30.0):
+    # Create our video & scene managers, then add the detector.
+    video_manager = VideoManager([video_path])
+    scene_manager = SceneManager()
+    scene_manager.add_detector(
+        ContentDetector(threshold=threshold))
+
+    # Improve processing speed by downscaling before processing.
+    video_manager.set_downscale_factor()
+
+    # Start the video manager and perform the scene detection.
+    video_manager.start()
+    scene_manager.detect_scenes(frame_source=video_manager)
+
+    # Each returned scene is a tuple of the (start, end) timecode.
+    list = scene_manager.get_scene_list()
+    save_images(list, video_manager, output_dir=outputURL)
 
 def checkFaceToTarget(know_imageURL, unknown_imageURL):
   known_image = face_recognition.load_image_file(know_imageURL)
